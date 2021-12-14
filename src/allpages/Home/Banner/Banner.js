bd0c6185b46bb64eff15060a1e756f60833d34f4
Hasibul0211 <mdhasibul0211@gmail.com>
2021-12-14 17:45:53 +0600

    banner complete

--- src/allpages/Home/Banner/Banner.js
+++ src/allpages/Home/Banner/Banner.js
@@ -6,58 +6,56 @@ import TypeWriterEffect from 'react-typewriter-effect';
 
 const Banner = () => {
     return (
-        <div>
-            <section className="banner-container">
-                <div className="banner-left">
-                    <h1 className="greeting">Welcome!!!!!</h1>
-                    <h2 className="title-name">md hasibul hasan</h2>
-                    <p className="typeeffect">
-                        <TypeWriterEffect
-                            textStyle={{
-                                fontFamily: 'Merriweather'
-                            }}
-                            startDelay={100}
-                            cursorColor="#161853"
-                            text="A Front-End Developer"
-                            typeSpeed={200}
-                            loop={true}
-                        />
-                    </p>
-                    <p className="typeeffect">
-                        <TypeWriterEffect
-                            textStyle={{
-                                fontFamily: 'Merriweather'
-                            }}
-                            startDelay={100}
-                            cursorColor="#161853"
-                            text="and"
-                            typeSpeed={200}
-                            loop={true}
-                        />
-                    </p>
-                    <p className="typeeffect">
-                        <TypeWriterEffect
-                            textStyle={{
-                                fontFamily: 'Merriweather'
-                            }}
-                            startDelay={100}
-                            cursorColor="#161853"
-                            text="A Digital Marketer"
-                            typeSpeed={200}
-                            loop={true}
-                        />
-                    </p>
+        <section className="banner-container">
+            <div className="banner-left">
+                <h1 className="greeting">Welcome!!!!!</h1>
+                <h2 className="title-name">md hasibul hasan</h2>
+                <p className="typeeffect">
+                    <TypeWriterEffect
+                        textStyle={{
+                            fontFamily: 'Merriweather'
+                        }}
+                        startDelay={100}
+                        cursorColor="#161853"
+                        text="A Front-End Developer"
+                        typeSpeed={200}
+                        loop={true}
+                    />
+                </p>
+                <p className="typeeffect">
+                    <TypeWriterEffect
+                        textStyle={{
+                            fontFamily: 'Merriweather'
+                        }}
+                        startDelay={100}
+                        cursorColor="#161853"
+                        text="and"
+                        typeSpeed={200}
+                        loop={true}
+                    />
+                </p>
+                <p className="typeeffect">
+                    <TypeWriterEffect
+                        textStyle={{
+                            fontFamily: 'Merriweather'
+                        }}
+                        startDelay={100}
+                        cursorColor="#161853"
+                        text="A Digital Marketer"
+                        typeSpeed={200}
+                        loop={true}
+                    />
+                </p>
 
-                </div>
-                <div className="banner-right">
-                    <div className="first-div-img">
-                        <div className="second-div-img">
-                            <img src={banner} alt="banner img" />
-                        </div>
+            </div>
+            <div className="banner-right">
+                <div className="first-div-img">
+                    <div className="second-div-img">
+                        <img src={banner} alt="banner img" />
                     </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     );
 };
 
